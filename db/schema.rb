d62ded1895b41c0ebc017732f2389a6fddab970d
Antoine Pultier <antoine.pultier@sintef.no>
2016-02-26 14:57:52 +0100

    Fix #2

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160225091912) do
+ActiveRecord::Schema.define(version: 20160226135438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,11 +19,11 @@ ActiveRecord::Schema.define(version: 20160225091912) do
 
   create_table "api_keys", force: :cascade do |t|
     t.integer  "user_id"
-    t.boolean  "enabled"
+    t.boolean  "enabled",    default: false, null: false
     t.string   "name"
     t.string   "key"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
   add_index "api_keys", ["key"], name: "index_api_keys_on_key", using: :btree
@@ -47,11 +47,11 @@ ActiveRecord::Schema.define(version: 20160225091912) do
   create_table "catalogues", force: :cascade do |t|
     t.integer  "user_id"
     t.string   "name"
-    t.boolean  "public"
+    t.boolean  "public",      default: false, null: false
     t.integer  "stars_count", default: 0
     t.string   "slug"
-    t.datetime "created_at",              null: false
-    t.datetime "updated_at",              null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
   end
 
   add_index "catalogues", ["slug"], name: "index_catalogues_on_slug", using: :btree
@@ -122,12 +122,12 @@ ActiveRecord::Schema.define(version: 20160225091912) do
 
   create_table "things", force: :cascade do |t|
     t.integer  "user_id"
-    t.boolean  "public"
+    t.boolean  "public",            default: false, null: false
     t.integer  "stars_count",       default: 0
     t.string   "name"
     t.string   "type"
-    t.datetime "created_at",                    null: false
-    t.datetime "updated_at",                    null: false
+    t.datetime "created_at",                        null: false
+    t.datetime "updated_at",                        null: false
     t.string   "slug"
     t.string   "file_id"
     t.integer  "file_size"
