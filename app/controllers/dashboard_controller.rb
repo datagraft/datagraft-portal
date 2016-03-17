859d290d0091a080a72596bd12682f84e0869c7d
Antoine Pultier <antoine.pultier@sintef.no>
2016-03-17 08:50:17 +0100

    the activity feed shows all public things (for now)
    user followings will eventually come later

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -51,16 +51,20 @@ class DashboardController < ApplicationController
     end
 
     # get user stars
-    stars = current_user.stars.order(created_at: :desc).includes(:thing).limit(10)
+    stars = Star.order(created_at: :desc).includes(:thing).where('things.public' => true).limit(10)
+    # TODO public stars on catalogues on followed users
     catalogue_stars = current_user.catalogue_stars.order(created_at: :desc).includes(:catalogue).limit(10)
 
     # bonsoir bonsoir 
     # добър вечер, драги приятелю
-    publicThings = Thing.where(user: current_user, public: true).order(updated_at: :desc).includes(:user).limit(10)
+    # publicThings = Thing.where(user: current_user, public: true).order(updated_at: :desc).includes(:user).limit(10)
+    publicThings = Thing.where(public: true).order(updated_at: :desc).includes(:user).limit(10)
     publicCatalogues = Catalogue.where(user: current_user, public: true).order(updated_at: :desc).includes(:user).limit(10)
     @activityFeed = (stars | publicThings | catalogue_stars | publicCatalogues).sort do |a,b|
       b[:updated_at] <=> a[:updated_at]
     end
+
+    @activityFeed = @activityFeed.take(15)
 
     authorize! :read, @things
   end
