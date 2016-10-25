095b815899cb73bbed8b1268f101ae35426d59d6
Brian Elvesæter <brian.elvesater@sintef.no>
2016-10-25 17:53:04 +0200

    Cleaned up SPARQL endpoint views. Fixed issue with repository size.

--- app/helpers/sparql_endpoints_helper.rb
+++ app/helpers/sparql_endpoints_helper.rb
@@ -4,14 +4,17 @@ module SparqlEndpointsHelper
     return "/"+current_user.username+"/sparql_endpoints/new#{ "?#{parameters.to_query}" if parameters.present? }"
   end
 
-  def repository_size2
-    current_user.get_ontotext_repository_size(@thing.uri)
-  end
-  
   def repository_size
+    if @thing.public
+      repository_size_of_public_endpoint
+    else  
+      current_user.get_ontotext_repository_size(@thing.uri)
+    end
+  end
+
+  def repository_size_of_public_endpoint
     return 'unknown number of' if not @thing.uri
     
-#    connect = Faraday.new(@thing.uri+'/size')  
     connect = Faraday.new
     resp_size = connect.get do |req|
       req.url @thing.uri+'/size'
@@ -22,15 +25,11 @@ module SparqlEndpointsHelper
     throw ("Unable to get size of the Ontotext Repository - " + resp_size.body + " - " + resp_size.status) unless 
     resp_size.status.between?(200, 299)
     
-    return resp_size.body
-  end  
+    return resp_size.body    
+  end
 
   def all_queries
     return Thing.where(type: 'Query')
   end
 
-  def execute_query(query)
-    return 'test'
-  end
-  
 end
