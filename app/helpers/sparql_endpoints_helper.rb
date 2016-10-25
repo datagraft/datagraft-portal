1bcf3151168f791b0e7c8eb2954ed5af860e2ebf
Brian Elvesæter <brian.elvesater@sintef.no>
2016-10-25 17:18:16 +0200

    Added new helper method for repository size that does not require current user.

--- app/helpers/sparql_endpoints_helper.rb
+++ app/helpers/sparql_endpoints_helper.rb
@@ -4,9 +4,26 @@ module SparqlEndpointsHelper
     return "/"+current_user.username+"/sparql_endpoints/new#{ "?#{parameters.to_query}" if parameters.present? }"
   end
 
-  def repository_size
+  def repository_size2
     current_user.get_ontotext_repository_size(@thing.uri)
   end
+  
+  def repository_size
+    return 'unknown number of' if not @thing.uri
+    
+#    connect = Faraday.new(@thing.uri+'/size')  
+    connect = Faraday.new
+    resp_size = connect.get do |req|
+      req.url @thing.uri+'/size'
+      req.headers['Content-Type'] = 'application/ld+json'
+      req.options.timeout = 720
+    end  
+    
+    throw ("Unable to get size of the Ontotext Repository - " + resp_size.body + " - " + resp_size.status) unless 
+    resp_size.status.between?(200, 299)
+    
+    return resp_size.body
+  end  
 
   def all_queries
     return Thing.where(type: 'Query')
