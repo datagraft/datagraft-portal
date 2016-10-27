b78bf52044f7131745e0a849774ff1379e0feebb
Steffen Dalgard <steffen.dalgard@sintef.no>
2016-10-27 11:11:31 +0200

    Added goto-endpoint for query page

--- app/assets/javascripts/queries.js
+++ app/assets/javascripts/queries.js
@@ -1,12 +1,13 @@
-redirectToSparqlEndpoint = function(obj, className){
-    var new_val = "inline";
-    if (obj.checked == false) new_val = 'none';
-
-    var x = document.querySelectorAll(className);
-    //console.log("updateFilterPublic " + obj.checked + " " + x.length + " " +x[0].style.display);
-    var i;
+redirectToSparqlEndpoint = function(base_url, select_id, select_slug){
+    console.log(" base_url " + base_url + " select_id " + select_id + " select_slug " +select_slug);
+    var obj_id = document.getElementById(select_id);
+    var sel_id_idx = obj_id.selectedIndex;
+    var sel_id_val = obj_id[sel_id_idx].value;
+    var obj_slug = document.getElementById(select_slug);
+    var sel_slug_val = obj_slug[sel_id_idx].value;
+    var redir_url = base_url + '/' + sel_slug_val;
+    console.log(" idx " + sel_id_idx + " id_val " + sel_id_val + " slug_val " +sel_slug_val);
+    console.log("Redirect to " + redir_url);
     //debugger;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = new_val;
-    }
+    window.location = redir_url;
 }
