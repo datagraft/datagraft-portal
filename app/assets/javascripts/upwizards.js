85955c75d0c366292f6754452cb9210da44f6533
Brian Elvesæter <brian.elvesater@sintef.no>
2016-11-03 14:23:14 +0100

    Refactored code and styles for Squeezebox and updated sparql endpoint view accordingly.

--- app/assets/javascripts/upwizards.js
+++ app/assets/javascripts/upwizards.js
@@ -23,19 +23,6 @@ $(document).ready(function(){
 
 });
 
-updateFilterPublic = function(obj, className){
-    var new_val = "inline";
-    if (obj.checked == false) new_val = 'none';
-
-    var x = document.querySelectorAll(className);
-    //console.log("updateFilterPublic " + obj.checked + " " + x.length + " " +x[0].style.display);
-    var i;
-    //debugger;
-    for (i = 0; i < x.length; i++) {
-      x[i].style.display = new_val;
-    }
-}
-
 updateFilterPublicStyle = function(obj, className, blockStyle){
     var new_val = blockStyle;
     if (obj.checked == false) new_val = 'none';
