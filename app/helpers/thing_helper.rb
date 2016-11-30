09c8f4354e74d09df568068d862e65df7c9ab961
Steffen Dalgard <steffen.dalgard@sintef.no>
2016-11-30 13:49:44 +0100

    Gen list: Enhanced names and added comments for usage

--- app/helpers/thing_helper.rb
+++ app/helpers/thing_helper.rb
@@ -45,7 +45,7 @@ module ThingHelper
 
 
   def update_asset_version_metric(asset_type_string)
-    
+
     begin
       num_assets = 0
       case asset_type_string
@@ -84,7 +84,7 @@ module ThingHelper
       num_versions.set({asset_type: asset_type_string}, num_assets)
     rescue Exception => e
       puts 'Error updating num_versions metric: '
-      puts e.message  
+      puts e.message
       puts e.backtrace
     end
 
@@ -102,4 +102,24 @@ module ThingHelper
 
     "/#{user.username}/#{classname.underscore.pluralize}/#{slug}#{method}#{ "?#{parameters.to_query}" if parameters.present? }"
   end
+
+  def thing_icon_name(thing)
+    ret = "help"
+    thing_type = thing.class.model_name.param_key
+    if thing_type == "data_page"
+      ret = "icon--data_page.svg"
+    elsif thing_type == "filestore"
+      ret = "icon--data_page.svg"
+    elsif thing_type == "query"
+      ret = "icon--query.svg"
+    elsif thing_type == "transformation"
+      ret = "icon--transformation.svg"
+    elsif thing_type == "data_store"
+      ret = "icon--queriable_data_store.svg"
+    elsif thing_type == "sparql_endpoint"
+      ret = "icon--queriable_data_store.svg"
+    end
+    return ret
+  end
+
 end
