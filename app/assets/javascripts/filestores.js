509a0d51d0a45e232df8a0620337720ab32ebe4c
Steffen Dalgard <steffen.dalgard@sintef.no>
2017-01-09 15:31:10 +0100

    Added pagination to file preview. Preview first 100 rows

--- app/assets/javascripts/filestores.js
+++ app/assets/javascripts/filestores.js
@@ -1,7 +1,23 @@
 document.addEventListener('turbolinks:load', function() {
-  $('#filestore-preview-table').DataTable({
-      paging: false,
+  var oTable,
+      $table;
+  $table = $('#filestore-preview-table');
+
+  $table.DataTable({
+      'lengthMenu': [[10, 25, 50, -1], [10, 25, 50, 'All']],
+      responsive: true,
+      "searching": true,
       pagingType: 'full_numbers',
+      dom: 'lrti<"mdl-card__actions mdl-card--border"p>',
+      scroller: true,
+      scrollCollapse: true,
+      scrollY: '57vh',
       "order": [],
   });
+
+  oTable = $table.DataTable();
+  $('#preview-search').keyup(function () {
+    oTable.search($(this).val()).draw();
+  });
+
 });
