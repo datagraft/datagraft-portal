1f47fd4e61cc9ce3b78bc837903f321a333d8eb6
Steffen Dalgard <steffen.dalgard@sintef.no>
2017-04-07 19:09:07 +0200

    Reworked dialog for large screenshots

--- app/assets/javascripts/landingpage.js
+++ app/assets/javascripts/landingpage.js
@@ -35,41 +35,41 @@ document.addEventListener('turbolinks:load', function() {
   });
 
 
-  var dialog1 = document.querySelector('#dialog1');
-  var dialog2 = document.querySelector('#dialog2');
-  var dialog3 = document.querySelector('#dialog3');
-  dialog1.style.width = '90%';
-  dialog2.style.width = '90%';
-  dialog3.style.width = '90%';
-  if (! dialog1.showModal) {
-    dialogPolyfill.registerDialog(dialog1);
+  var show_ovl1 = document.querySelector('#show-ovl1');
+  if(show_ovl1) {
+    show_ovl1.addEventListener('click', function() {
+      document.querySelector("#ovl1").style.display = "block";
+    });
   }
-  if (! dialog2.showModal) {
-    dialogPolyfill.registerDialog(dialog2);
+  var close_ovl1 = document.querySelector('#close-ovl1');
+  if(close_ovl1) {
+    close_ovl1.addEventListener('click', function() {
+      document.querySelector("#ovl1").style.display = "none";
+    });
   }
-  if (! dialog3.showModal) {
-    dialogPolyfill.registerDialog(dialog3);
+  var show_ovl2 = document.querySelector('#show-ovl2');
+  if(show_ovl2) {
+    show_ovl2.addEventListener('click', function() {
+      document.querySelector("#ovl2").style.display = "block";
+    });
   }
-  var showDialog1Button = document.querySelector('#show-dialog1');
-  var showDialog2Button = document.querySelector('#show-dialog2');
-  var showDialog3Button = document.querySelector('#show-dialog3');
-  showDialog1Button.addEventListener('click', function() {
-    dialog1.showModal();
-  });
-  showDialog2Button.addEventListener('click', function() {
-    dialog2.showModal();
-  });
-  showDialog3Button.addEventListener('click', function() {
-    dialog3.showModal();
-  });
-  dialog1.querySelector('.close').addEventListener('click', function() {
-    dialog1.close();
-  });
-  dialog2.querySelector('.close').addEventListener('click', function() {
-    dialog2.close();
-  });
-  dialog3.querySelector('.close').addEventListener('click', function() {
-    dialog3.close();
-  });
+  var close_ovl2 = document.querySelector('#close-ovl2');
+  if(close_ovl2) {
+    close_ovl2.addEventListener('click', function() {
+      document.querySelector("#ovl2").style.display = "none";
+    });
+  }
+  var show_ovl3 = document.querySelector('#show-ovl3');
+  if(show_ovl3) {
+    show_ovl3.addEventListener('click', function() {
+      document.querySelector("#ovl3").style.display = "block";
+    });
+  }
+  var close_ovl3 = document.querySelector('#close-ovl3');
+  if(close_ovl3) {
+    close_ovl3.addEventListener('click', function() {
+      document.querySelector("#ovl3").style.display = "none";
+    });
+  }
 
 });
