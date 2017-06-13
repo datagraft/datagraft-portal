c35a621aa272b8b79a7969b9353b6caf56ae776a
Nikolay Nikolov <nikolay.nikolov@sintef.no>
2017-06-13 15:30:43 +0200

    Fixed and configured mailer. Updated password reset UI

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -13,22 +13,22 @@ Rails.application.configure do
   # Show full error reports.
   config.consider_all_requests_local = true
 
-#  # Enable/disable caching. By default caching is disabled.
-#  if Rails.root.join('tmp/caching-dev.txt').exist?
-#    config.action_controller.perform_caching = true
-#
-#    config.cache_store = :memory_store
-#    config.public_file_server.headers = {
-#      'Cache-Control' => 'public, max-age=172800'
-#    }
-#  else
-#    config.action_controller.perform_caching = false
-#
-#    config.cache_store = :null_store
-#  end
+  #  # Enable/disable caching. By default caching is disabled.
+  #  if Rails.root.join('tmp/caching-dev.txt').exist?
+  #    config.action_controller.perform_caching = true
+  #
+  #    config.cache_store = :memory_store
+  #    config.public_file_server.headers = {
+  #      'Cache-Control' => 'public, max-age=172800'
+  #    }
+  #  else
+  #    config.action_controller.perform_caching = false
+  #
+  #    config.cache_store = :null_store
+  #  end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
@@ -56,14 +56,42 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.action_mailer.default
+  
+  # Configure the default host and port for the mailer
+  deploy_host = ENV["DATAGRAFT_DEPLOY_HOST"] ? ENV["DATAGRAFT_DEPLOY_HOST"] : 'localhost'
+  deploy_port = ENV["DATAGRAFT_DEPLOY_PORT"]
 
   # URL in emails
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  if deploy_port then
+    # if we provided the port - add to configuration
+    config.action_mailer.default_url_options = { host: deploy_host, port: deploy_port}
+  else
+    # if no port was specified - only use the host name (e.g., in a live deployment)
+    config.action_mailer.default_url_options = { host: deploy_host}
+  end
 
   config.web_console.whitelisted_ips = ENV['WEB_CONSOLE_WHITELISTED_IPS'] or '10.0.0.0/8'
+  #=========================
+  # ==> Mailer Configuration
+  # Configure the e-mail address which will be shown in Devise::Mailer,
+  # note that it will be overwritten if you use your own mailer class
+  # with default "from" parameter.
+  config.mailer_sender = ENV["DEVISE_MAILER_SENDER"] or 'datagraft@sintef.no'
+  
+  #=========================
+  config.action_mailer.default_options = {from: 'datagraft@sintef.no'}
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "email-smtp.eu-west-1.amazonaws.com",
+    :port => 587,
+    :user_name => ENV["SES_SMTP_USERNAME"],
+    :password => ENV["SES_SMTP_PASSWORD"],
+    :authentication => :login,
+    :enable_starttls_auto => true
+    }
+
 end
