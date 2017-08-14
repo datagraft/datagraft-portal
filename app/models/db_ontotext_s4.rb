56fdf3fc1ba338365315259dc0c6aab1d0f994ea
Brian Elvesæter <brian.elvesater@sintef.no>
2017-08-14 15:32:49 +0200

    Added new module for S4.

---
+++ app/models/db_ontotext_s4.rb
@@ -0,0 +1,32 @@
+class DbOntotextS4 < DbAccount
+   
+    def delete()
+    end
+    
+    def new_repository(thing)
+    end
+    
+    def upload_file_to_repository(db_repository, file, file_type)
+    end
+
+  def query_repository(repo_hash, query_string)
+  end
+
+  def delete_repository(repo_hash)
+  end
+
+  def add_key(key, name)
+  end
+
+  def generate_key(db_key)
+  end
+
+  def delete_key(db_key)
+  end
+
+  def update_key(db_key)
+  end
+
+    
+    
+end
