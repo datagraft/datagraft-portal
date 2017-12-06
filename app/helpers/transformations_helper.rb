1210aa95557010d1a7e8192b3d0133c9e4ade3d6
Steffen Dalgard <steffen.dalgard@sintef.no>
2017-12-06 14:16:55 +0100

    Added download of transformation code

--- app/helpers/transformations_helper.rb
+++ app/helpers/transformations_helper.rb
@@ -10,11 +10,15 @@ module TransformationsHelper
   def new_transformation_path
     "/#{current_user.username}/transformations/new"
   end
-  
+
+  def transformation_download_path(transformation_input)
+    "#{transformation_path(transformation_input)}/download"
+  end
+
   def transformations_path
     return "/explore" unless user_signed_in?
-    
+
     return "/#{current_user.username}/transformations"
   end
-  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -55,6 +55,7 @@ Rails.application.routes.draw do
 
  post ':username/transformations/:id/execute/:type/' => 'transformations#execute'
  post ':username/transformations/:id/execute/:type/:file_id' => 'transformations#execute'
+  get ':username/transformations/:id/download' => 'transformations#download'
 
   post ':username/queries/:id/execute/:qds_username/:qds_id' => 'queries#execute'
   get 'querying' => 'queries#execute'
