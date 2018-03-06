4ea78531651e7a55ca46008634d3e2387672e11b
Steffen Dalgard <steffen.dalgard@sintef.no>
2018-03-06 16:04:33 +0100

    Added readout of ArangoDb version
    Added JWT Auth but not enabled

--- app/controllers/dbms_controller.rb
+++ app/controllers/dbms_controller.rb
@@ -6,6 +6,16 @@ class DbmsController < ApplicationController
     @dbm_s4s = current_user.search_for_existing_dbms_type("DbmS4")
     @dbm_ontotext_legs = current_user.search_for_existing_dbms_type("DbmOntotextLeg")
     @dbm_arangos = current_user.search_for_existing_dbms_type("DbmArango")
+
+    @dbm_arangos_info = {}
+    @dbm_arangos.each do |dbm_arango|
+      begin
+        info = dbm_arango.get_version(false)
+        @dbm_arangos_info[dbm_arango.id] = "Ver: #{info[:version]}"
+      rescue => e
+        puts e.message
+      end
+    end
   end
 
   def index_things
